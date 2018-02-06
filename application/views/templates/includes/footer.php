b491ce104388ac39e2c2821291b63f905ea03948
techpulsetoday <techpulsetoday@gmail.com>
2018-02-06 22:52:15 +0530

    Back To Top

--- application/views/templates/includes/footer.php
+++ application/views/templates/includes/footer.php
@@ -1,4 +1,5 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<button onclick="topFunction()" id="back-to-top" title="Go to top">Top</button>
 <div class="container-fluid">
     <footer>
         <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
--- assets/js/script.js
+++ assets/js/script.js
@@ -37,4 +37,21 @@ $(document).ready(function () {
             }, 1000);
         }
     });
+    /* Back To Top */
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    /* scroll body to 0px on click */
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+    $('#back-to-top').tooltip('show');
 });
